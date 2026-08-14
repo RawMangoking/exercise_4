79ab0958c6e3176e60a3dab54750623f34a0f7b6
RawMangoking <narenkarthik21122005tnj@gmail.com>
2026-08-14 23:21:40 +0530

    Deleted attendance

--- attendance.py
+++
@@ -1,7 +0,0 @@
-﻿def attendance(student):
-    if student:
-        print("Student attendance marked")
-    else:
-        print("Student not present")
-attendance(student=True)
-# WIP: refining logic, not done yet
